a02a69087d5ee98535bc32110e04d50b9d731a37
yarella <yarellavictoriahenriquezb@gmail.com>
2023-09-07 12:24:57 -0300

    sql basico

---
+++ 01-basic/03-insert-movies.sql
@@ -0,0 +1,4 @@
+\c movies 
+INSERT INTO movies (genere, title, year, director) VALUES('Terror', 'Los pitufos', 2011, 'gargamel');
+INSERT INTO movies (genere, title, year, director) VALUES('Terror', 'El pie', 2011, 'Producida por el pie');
+INSERT INTO movies (genere, title, year, director) VALUES('Terror', 'Mit Chiken', 2011,'Chiken gun');
